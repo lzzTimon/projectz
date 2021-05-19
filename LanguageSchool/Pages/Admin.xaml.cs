07d2d74bc31cab4bc9c635e9f01529bab2d26a5b
Timofey Esin <timoxa52@outlook.com>
2021-05-19 13:26:05 +0300

    2 изменение

--- LanguageSchool/Pages/Admin.xaml.cs
+++ LanguageSchool/Pages/Admin.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using Button = System.Windows.Controls.Button;
+using MessageBox = System.Windows.MessageBox;
+
 
 namespace LanguageSchool.Pages
 {
@@ -60,15 +66,36 @@ namespace LanguageSchool.Pages
             }
         }
 
+        /// <summary>
+        /// Вывод данных из БД в textbox, при нажатии кнопки изменить
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BEdit_Click(object sender, RoutedEventArgs e)
         {
+            SVGrid.Visibility = Visibility.Collapsed;
+            StackChange.Visibility = Visibility.Visible;
+            StackAdd.Visibility = Visibility.Collapsed;
+
             Button BtnEdit = (Button)sender;
             int ind = Convert.ToInt32(BtnEdit.Uid);
             Service S = ServiceList[ind];
-            MessageBox.Show(S.Title);
+
+            TextID.Text = Convert.ToString(S.ID);
+            BoxTitle.Text = Convert.ToString(S.Title);
+            BoxCost.Text = Convert.ToInt32(S.Cost)+"";
+            BoxTime.Text = Convert.ToInt32(S.DurationInSeconds/60)+"";
+            BoxDescription.Text = Convert.ToString(S.Description);
+            BoxDiscount.Text = Convert.ToDouble(S.Discount*100)+"";
+            ForPath.Text = Convert.ToString(S.MainImagePath);
 
         }
 
+        /// <summary>
+        /// Выделение ячеек со скидкой
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void StackPanel_Initialized(object sender, EventArgs e)
         {
             if (i < ServiceList.Count)
@@ -83,6 +110,11 @@ namespace LanguageSchool.Pages
 
         }
 
+        /// <summary>
+        /// Вывод данных в таблицу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TextBlock_Initialized_1(object sender, EventArgs e)
         {
             if (i < ServiceList.Count)
@@ -119,6 +151,130 @@ namespace LanguageSchool.Pages
             if (S.Discount > 0)
             {
                 TB.Text = Convert.ToInt32(S.Cost) + " ";
+            }
+        }
+
+        /// <summary>
+        /// Удаление записей 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BDelete_Initialized(object sender, EventArgs e)
+        {
+            Button BtnDel = (Button)sender;
+            if (BtnDel != null)
+            {
+                BtnDel.Uid = Convert.ToString(i);
+            }
+        }
+
+        private void BDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Button BtnDel = (Button)sender;
+            int ind = Convert.ToInt32(BtnDel.Uid);
+            Service S = ServiceList[ind];
+
+            DialogResult DR = (DialogResult)MessageBox.Show("Внимание!", "Следующая запись будет удалена. Удалить запись?", (MessageBoxButton)MessageBoxButtons.YesNo);
+            if (DR == DialogResult.Yes)
+            {
+                Classes.Base.Ent.Service.Remove(S);
+                MessageBox.Show("Запись удалена");
+                Classes.Base.Ent.SaveChanges();
+                Classes.Global.GlobalFrame.Navigate(new Admin());
+            }
+            else if (DR == DialogResult.No)
+            {
+                MessageBox.Show("Изменения не внесены");
+            }
+            
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.Global.GlobalFrame.Navigate(new Admin());
+        }
+
+        private void BtnImg_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog OFD = new System.Windows.Forms.OpenFileDialog();
+            OFD.ShowDialog();
+            string Path = OFD.FileName;
+            if (Path != null)
+            {
+                int c = Path.IndexOf('У');
+                string New = Path.Substring(c);
+                ForPath.Text = New.ToString();
+            }
+        }
+
+        private void BtnWrite_Click(object sender, RoutedEventArgs e)
+        {
+            double discount = Convert.ToDouble(BoxDiscount.Text)/100;
+            int time = Convert.ToInt32(BoxTime.Text)*60;
+
+            DialogResult DR = (DialogResult)MessageBox.Show("Следующая запись будет изменена. Изменить запись?", "Внимание",  (MessageBoxButton)MessageBoxButtons.YesNo);
+            if (DR == DialogResult.Yes)
+            {
+                Service obj = new Service() { ID = Convert.ToInt32(TextID.Text), Title = BoxTitle.Text, Cost = Convert.ToInt32(BoxCost.Text), DurationInSeconds = time, Description = BoxDescription.Text, Discount = discount, MainImagePath = ForPath.Text };
+                Classes.Base.Ent.Service.AddOrUpdate(obj);
+                Classes.Base.Ent.SaveChanges();
+                MessageBox.Show("Изменения сохранены");
+            }
+            else if (DR == DialogResult.No)
+            {
+                MessageBox.Show("Изменения не были сохранены");
+            }
+        }
+
+        private void AddRecord_Click(object sender, RoutedEventArgs e)
+        {
+            StackAdd.Visibility = Visibility.Visible;
+            StackChange.Visibility = Visibility.Collapsed;
+            SVGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnAddImg_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog OFD = new System.Windows.Forms.OpenFileDialog();
+            OFD.ShowDialog();
+            string Path = OFD.FileName;
+            if (Path != null)
+            {
+                int c = Path.IndexOf('У');
+                string New = Path.Substring(c);
+                ForPath.Text = New.ToString();
+            }
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Service S = ServiceList[i];
+            if (S.Title == BoxNewHeader.Text)
+            {
+                MessageBox.Show("Данное имя уже существует в системе");
+            }
+            if (Convert.ToInt32(BoxNewTime.Text) > 14400 || Convert.ToInt32(BoxNewTime.Text) < 0)
+            {
+                MessageBox.Show("Время не может привышать 4 часов или быть отрицательным");
+            }
+            else if (Convert.ToInt32(BoxNewTime.Text) < 14400 || Convert.ToInt32(BoxNewTime.Text) > 0)
+            {
+                double discount = Convert.ToDouble(BoxNewDiscount.Text) / 100;
+                int time = Convert.ToInt32(BoxNewTime.Text) * 60;
+
+                DialogResult DR = (DialogResult)MessageBox.Show("Следующая запись будет добавлена. Добавить запись?", "Внимание", (MessageBoxButton)MessageBoxButtons.YesNo);
+                if (DR == DialogResult.Yes)
+                {
+                    Service obj = new Service() { Title = BoxNewHeader.Text, Cost = Convert.ToInt32(BoxNewCost.Text), DurationInSeconds = time, Description = BoxNewDescription.Text, Discount = discount, MainImagePath = AddPath.Text };
+                    Classes.Base.Ent.Service.Add(obj);
+                    Classes.Base.Ent.SaveChanges();
+                    MessageBox.Show("Запись добавлена");
+                }
+                else if (DR == DialogResult.No)
+                {
+                    MessageBox.Show("Запись не была добавлена");
+                }
+
             }
         }
     }
